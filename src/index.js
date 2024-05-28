cb913f78f6d8b043357eaddc48a123ec78b048d3
Inku <inkrak@hotmail.com>
2024-05-28 17:04:40 +0700

    deploy test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { createBrowserRouter, RouterProvider,Route, Link } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Route, Link } from 'react-router-dom'
 import Home from "./Components/Home.jsx";
 import Login from "./Components/Login"
 import AddNewProduct from "./Components/addNewProduct"
@@ -13,35 +13,36 @@ import Productdata from "./Components/Productdata.jsx"
 import { AuthProvider } from './auth/AuthContext';
 
 const router = createBrowserRouter([
-    {
-      path:"/",
-      element: <App/>
-    },
-    {
-      path:"/login",
-      element: <Login/>
-    },
-    {
-      path:"/addNewProduct",
-      element: <AddNewProduct/>
-    },
-    {
-      path: "/products/:productId",
-      element: <Productdata />,
-    },
-    {
-      path: "/register",
-      element: <Register />,
-    },
-  ])
-  
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <AuthProvider>
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/addNewProduct",
+    element: <AddNewProduct />
+  },
+  {
+    path: "/products/:productId",
+    element: <Productdata />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+])
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthProvider>
       <RouterProvider router={router} />
-      </AuthProvider>
-    </React.StrictMode>,
-)
+    </AuthProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
